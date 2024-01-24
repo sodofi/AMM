c0cdcf7198fcde3bf0e0bd7bc1bd9a88401d6979
sodofi <59624772+sodofi@users.noreply.github.com>
2024-01-24 15:27:06 -0600

    Update exchange.sol

--- exchange.sol
+++ exchange.sol
@@ -7,7 +7,7 @@ import "hardhat/console.sol";
 
 
 contract TokenExchange is Ownable {
-    string public exchange_name = 'BJCEX';
+    string public exchange_name = 'COCEX';
 
     address tokenAddr = 0x5FbDB2315678afecb367f032d93F642f64180aa3;                                  // TODO: paste token contract address here
     Token public token = Token(tokenAddr);                                
